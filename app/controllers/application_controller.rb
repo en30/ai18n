99ecf69d9f47eff3dca684ef1cd6ba2449167749
en30 <en30.git@gmail.com>
2024-09-27 17:35:37 +0900

    rails generate scaffold Post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    I18n.with_locale(http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale, &action)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Ai18n
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.default_locale = :ja
   end
 end
